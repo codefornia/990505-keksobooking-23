a8559a76be0b1c4c6591c15720631d97a1b39fce
codefornia <codeforniadreamin@gmail.com>
2021-06-15 08:44:13 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 const SIMILAR_APARTMENTS_COUNT = 10;
-const AVATAR_NUMBERS = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
+const TITLES = [
+  'Капучино Отель',
+  'Strawberry Duck',
+  'Халва Отель Полянка',
+  'Отель "Чик"',
+  'Чижик-пыжик Отель',
+  'Апартаменты "Борис"',
+  'Гостиница "Жемчужина"',
+  'Комната "Отдых"',
+  'Деревянный отель',
+  'Балкон и кроватка',
+];
+const MIX_PRICE = 0;
+const MAX_PRICE = 10000;
 const TYPE_APARTMENTS = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const MIN_ROOMS_QTY = 0;
+const MAX_ROOMS_QTY = 100;
+const MIN_GUESTS_QTY = 0;
+const MAX_GUESTS_QTY = 300;
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const DESCRIPTIONS = [
   'Гостевой дом расположен на берегу моря. В распоряжении гостей общая кухня и детская игровая площадка.',
@@ -14,18 +31,6 @@ const DESCRIPTIONS = [
   'Отель размещается в вилле с деревянным каркасом. Гостям отеля предоставляются номера в классическом стиле с телевизором.',
   'К услугам гостей номера с собственным балконом. Все номера оснащены телевизором с плоским экраном и кабельными каналами.',
 ];
-const TITLES = [
-  'Капучино Отель',
-  'Strawberry Duck',
-  'Халва Отель Полянка',
-  'Отель "Чик"',
-  'Чижик-пыжик Отель',
-  'Апартаменты "Борис"',
-  'Гостиница "Жемчужина"',
-  'Комната "Отдых"',
-  'Деревянный отель',
-  'Балкон и кроватка',
-];
 const CHECKIN = ['12:00', '13:00', '14:00'];
 const CHECKOUT = ['12:00', '13:00', '14:00'];
 const IMAGE_NAME = [
@@ -33,7 +38,18 @@ const IMAGE_NAME = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
-
+const MIN_LATITUDE = 35.65000;
+const MAX_LATITUDE = 35.70000;
+const MIN_LONGITUDE = 139.70000;
+const MAX_LONGITUDE = 139.80000;
+const getAvatarNumberArray = () => {
+  const arr = [];
+  for (let i = 1; i <= SIMILAR_APARTMENTS_COUNT; i++) {
+    arr.push(String(i).padStart(2, '0'));
+  }
+  return arr;
+};
+const avatarNumberArray = getAvatarNumberArray(SIMILAR_APARTMENTS_COUNT);
 const getRandomNumberInt = (firstNumber, secondNumber) => {
   if (firstNumber > secondNumber) {
     return 'Диапазон не верен, введите корректные данные';
@@ -58,8 +74,6 @@ const getRandomNumberInt = (firstNumber, secondNumber) => {
   const max = Math.floor(secondNumber);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-
-
 const getRandomNumberFloat = (firstNumber, secondNumber, numsAfterDot = 2) => {
   if (firstNumber > secondNumber) {
     return 'Диапазон не верен, введите корректные данные';
@@ -76,11 +90,7 @@ const getRandomNumberFloat = (firstNumber, secondNumber, numsAfterDot = 2) => {
   const powerNumber = Math.pow(10, numsAfterDot);
   return (Number((Math.random() * (secondNumber - firstNumber) + firstNumber).toFixed(numsAfterDot)) * powerNumber) / powerNumber;
 };
-
-const getRandomArrayElement = (elements) => elements[getRandomNumberInt(0, elements.length - 1)];
-
-const getRegularizedArrayElement = (elements) => elements.pop();
-
+const getRandomArrayElement = (arr) => arr[getRandomNumberInt(0, arr.length - 1)];
 const getShuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
@@ -90,43 +100,42 @@ const getShuffleArray = (array) => {
   }
   return array.slice(Math.random() * (array.length - 1));
 };
-
-const createAuthor = `avatar: img/avatars/user ${getRegularizedArrayElement(AVATAR_NUMBERS)} .png`;
-
-const createOffer = {
-  title: getRegularizedArrayElement(TITLES),
-  address: `${getRandomNumberFloat(335767, 345767, 2)}, ${getRandomNumberFloat(335767, 345767, 2)}`,
-  price: getRandomNumberInt(0, 10000),
+const createAuthor = () => ({avatar: `img/avatars/user${avatarNumberArray.pop()}.png`});
+const createOffer = () => ({
+  title: getRandomArrayElement(TITLES),
+  address: `${getRandomNumberFloat(MIN_LATITUDE, MAX_LATITUDE, 2)}, ${getRandomNumberFloat(MIN_LONGITUDE, MAX_LONGITUDE, 2)}`,
+  price: getRandomNumberInt(MIX_PRICE, MAX_PRICE),
   type: getRandomArrayElement(TYPE_APARTMENTS),
-  rooms: getRandomNumberInt(0, 100),
-  guests: getRandomNumberInt(0, 300),
+  rooms: getRandomNumberInt(MIN_ROOMS_QTY, MAX_ROOMS_QTY),
+  guests: getRandomNumberInt(MIN_GUESTS_QTY, MAX_GUESTS_QTY),
   checkin: getRandomArrayElement(CHECKIN),
   checkout: getRandomArrayElement(CHECKOUT),
   features: getShuffleArray(FEATURES),
-  description: getRegularizedArrayElement(DESCRIPTIONS),
+  description: DESCRIPTIONS.pop(),
   photos: getShuffleArray(IMAGE_NAME),
-};
+});
 
-const createLocation = {
-  lat: getRandomNumberFloat(35.65000, 35.70000, 5),
-  lng: getRandomNumberFloat(139.70000, 139.80000, 5),
-};
+const createLocation = () => ({
+  lat: getRandomNumberFloat(MIN_LATITUDE, MAX_LATITUDE, 5),
+  lng: getRandomNumberFloat(MIN_LONGITUDE, MAX_LONGITUDE, 5),
+});
 
-const createApartment = {
-  author: createAuthor,
-  offer: createOffer,
-  location: createLocation,
-};
+const createApartment = () => ({
+  author: createAuthor(),
+  offer: createOffer(),
+  location: createLocation(),
+});
+
 /*
-const similarApartments = (count)=> (new Array(count).fill(null).map(() => createApartment));
-similarApartments(SIMILAR_APARTMENTS_COUNT);*/
+const similarApartments = new Array(SIMILAR_APARTMENTS_COUNT).fill(null).map(() => createApartment());
+console.log(similarApartments);
+*/
 
-const getSimilarArray = (length, element) => {
+const getSimilarArray = (length) => {
   const array = [];
   for (let i = 0; i <= length - 1; i++) {
-    array.push(element);
+    array.push(createApartment());
   }
   return array;
 };
-
-getSimilarArray(SIMILAR_APARTMENTS_COUNT, createApartment);
+getSimilarArray(SIMILAR_APARTMENTS_COUNT);
