0d6569a715b3fd2cf67520524d34a26f0db2e001
codefornia <codeforniadreamin@gmail.com>
2021-07-01 18:22:42 +0300

    Внесены правки по Модуляции

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
 import {getSimilarArray, SIMILAR_APARTMENTS_COUNT} from './data.js';
-import './map.js';
-import './filter.js';
-import './form.js';
-import './card-popup.js';
 getSimilarArray(SIMILAR_APARTMENTS_COUNT);
 
