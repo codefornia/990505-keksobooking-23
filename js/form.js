3beaf35bf4d64edb074598cb7d42e23416ca5aa3
codefornia <codeforniadreamin@gmail.com>
2021-07-05 20:51:06 +0300

    внесены исправления Отрисуй меня полностью ч2

---
+++ js/form.js
@@ -0,0 +1,15 @@
+const adForm = document.querySelector('.ad-form');
+const fieldsetBlocks = adForm.querySelectorAll('fieldset');
+const disableAdForm = () => {
+  adForm.classList.add('ad-form--disabled');
+  fieldsetBlocks.forEach((fieldsetBlock) => {
+    fieldsetBlock.setAttribute('disabled', 'disabled');
+  });
+};
+const activeAdForm = () => {
+  adForm.classList.remove('ad-form--disabled');
+  fieldsetBlocks.forEach((fieldsetBlock) => {
+    fieldsetBlock.removeAttribute('disabled');
+  });
+};
+export {disableAdForm, activeAdForm};
--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 import {map, getDataCardsElements} from './card-popup.js';
 import {getSimilarArray, SIMILAR_APARTMENTS_COUNT} from './data.js';
+import {disableFilter, activeFilter} from './filter.js';
+import {disableAdForm, activeAdForm} from './form.js';
 const dataCards = getSimilarArray(SIMILAR_APARTMENTS_COUNT);
 const dataCardsElements = getDataCardsElements(dataCards);
 map.appendChild(dataCardsElements[0]);
+disableFilter();
+disableAdForm();
+activeFilter();
+activeAdForm();
