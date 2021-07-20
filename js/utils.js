a3d5d1740f09ff08af1dda2fc63268dea64173e9
codefornia <codeforniadreamin@gmail.com>
2021-07-20 20:27:58 +0300

    Надо подкачаться

--- js/utils.js
+++ js/utils.js
@@ -40,6 +40,4 @@ const getRandomNumberInt = (firstNumber, secondNumber) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-
-
 export {getRandomNumberFloat, getRandomNumberInt};
